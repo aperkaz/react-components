fbe2db6c2d1b4f9494740fcc4f7ad0b7016fe5d7
Alain Perkaz <alainperkaz@gmail.com>
2018-01-06 18:03:48 +0100

    Add AudioManager component

---
+++ src/components/AudioManager/_story.jsx
@@ -0,0 +1,22 @@
+// -- dependencies --
+import React from 'react';
+import { storiesOf, setAddon } from '@storybook/react';
+import { text } from '@storybook/addon-knobs/react';
+import { host } from 'storybook-host';
+import JSXAddon from 'storybook-addon-jsx';
+import { withNotes } from '@storybook/addon-notes';
+// -- app --
+import AudioManager from './default';
+
+setAddon(JSXAddon);
+
+storiesOf('Components/AudioManager', module)
+    .addDecorator(host({
+        title: 'CoverArt',
+        align: 'center middle',
+    }))
+    .addWithJSX('default',
+        withNotes('Component for playback online-audio streams')
+        (() =>
+            <AudioManager src={text('audio src', 'http://ice2.somafm.com/folkfwd-128-mp3')} />
+        ));
---
+++ src/components/AudioManager/default.jsx
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import 'font-awesome/css/font-awesome.min.css';
+
+const pauseIcon = 'fa fa-pause fa-2x';
+const playIcon = 'fa fa-play fa-2x';
+
+const A = styled.a`
+    color: #c7c7c7;
+    display: inline-block;
+    border-radius: 40px;
+    box-shadow: 0px 0px 2px #888;
+    padding: 0.5em 0.6em;
+`;
+
+
+class AudioManager extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            stream: null,
+            playing: false,
+            icon: playIcon
+        };
+    }
+
+    componentDidMount(){
+        this.playStream();
+    }
+
+    componentWillUnmount(){
+        this.pauseStream();
+    }
+
+    playStream(){
+        let stream = new Audio(this.props.src);
+        stream.load();
+        stream.volume = 1;
+        stream.play();
+
+        this.setState({
+            stream,
+            playing: true,
+            icon: pauseIcon,
+        });
+    }
+
+    pauseStream(){
+        this.state.stream.pause();
+        this.state.stream.src = "";
+
+        this.setState ({
+            stream: null,
+            playing: false,
+            icon: playIcon,
+        });
+    }
+
+
+    toggleStream() {
+        this.state.playing
+            ? this.pauseStream()
+            : this.playStream()
+    }
+
+    render(){
+      return (
+          <A
+              className={this.state.icon}
+              onClick={this.toggleStream.bind(this, null)}
+          ></A>
+      );
+    }
+};
+
+AudioManager.propTypes = {
+    src: PropTypes.string
+};
+
+
+export default AudioManager;
